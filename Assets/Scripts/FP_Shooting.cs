c446912310a100a51556b88183eb16c77b40aba2
Jaapster <ejboutens1337@gmail.com>
2017-12-13 09:25:36 +0100

    backup

---
+++ Assets/Scripts/FP_Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FP_Shooting : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+	float bulletImpulse = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Camera cam = Camera.main;
+			GameObject thebullet = (GameObject)Instantiate(bullet_prefab,cam.transform.position,cam.transform.rotation);
+			thebullet.rigidbody.AddForce(cam.transform.forward, bulletImpulse,ForceMode.Impulse);
+		}
+
+		
+	}
+}
